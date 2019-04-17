9f6b150188e54d01da99d52c1c7c7244a8603b45
Juanma <juanmasanchezortiz@hotmail.com>
2019-04-17 15:25:50 +1000

    added in methods to controller and before_action

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,47 @@
+class ListingsController < ApplicationController
+    before_action :set_listing, only: [:show, :edit, :update, :destroy]
+
+    #this is like a method with one positional argument and one name argument:
+    #before_action(:set_listing, only: [:show, :edit, :update, :destroy])
+    
+    def create
+        #create new listing
+    end
+
+    def index
+        #shows all listings
+        @listings = Listing.all 
+    end
+
+    def update 
+        #updates the current listing
+        
+    end
+
+    def edit
+        #show the edit
+    end
+
+    def destroy
+        #deletes current listing
+        
+    end
+
+    def new 
+        #shows form for creating a new listing
+    end
+
+    def show 
+        #shows a single listing
+        #params is a hash, so we still access a hash
+        
+    end
+
+    private 
+    #everything underneath this private word is not accessible outside of the object
+
+    def set_listing
+        id = params[:id]
+        @listing = Listing.find(id)
+    end
+end
