a2f812530942472c22532f2b546aa753be9da454
Juanma <juanmasanchezortiz@hotmail.com>
2019-05-01 12:36:02 +1000

    fixing bugs

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, path: "/", path_names: {sign_in: "login"}
   get "/", to: "pages#home", as: "root"
   resources :listings
+  get "/payments/success", to: "payments#success"
+  post "/payments", to: "payments#stripe"
+  
 end
